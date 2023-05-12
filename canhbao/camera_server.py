db1bb1a49ea18881476ef73716d32934dafc2210
thanghvtts <TTS.ThangHoang@vconnex.vn>
2023-05-12 12:49:57 +0700

    sua g iao dien map camera

--- canhbao/camera_server.py
+++ canhbao/camera_server.py
@@ -31,53 +31,10 @@ class Camera(BaseCamera):
                 b"OK"
             )  # this is needed for the stream to work with REQ/REP pattern
 
-            print("camera ID", cam_id)
+            # print("camera ID", cam_id)
             num_frames += 1
 
-        # from collections import defaultdict
-
-        # # Create a dictionary to store the last received frame for each camera
-        # last_frames = defaultdict(lambda: None)
-        # print("co last_frame", last_frames)
-
-        # # Define a timeout value (in seconds)
-        # timeout = 1
-
-        # while True:
-        #     time_start = time.time()
-
-        #     cam_id, frame = image_hub.recv_image()
-
-        #     image_hub.send_reply(
-        #         b"OK"
-        #     )  # this is needed for the stream to work with REQ/REP pattern
-
-        #     print("camera ID", cam_id)
-        #     num_frames += 1
-
-        #     # Check if the current camera has sent a frame before
-        #     if last_frames[cam_id] is not None:
-        #         # Calculate the time elapsed since the last frame was received
-        #         elapsed_time = time_start - last_frames[cam_id]
-                
-        #         # Check if the elapsed time exceeds the timeout value
-        #         if elapsed_time > timeout:
-        #             # Close the connection to the camera
-        #             image_hub.disconnect(cam_id)
-        #             last_frames[cam_id] = None
-        #             print(f"Connection to {cam_id} closed due to timeout")
-            
-        #     # Store the timestamp of the current frame for the current camera
-        #     last_frames[cam_id] = time_start
-    
-   
-
-            # detection fire
-            # Test = CameraDetectionFire()
-            
-     
             prediction, frame = detector.detection_fire(frame)
-            # print("==========================================prediction: ",prediction)
             localtime = datetime.datetime.now()
             height, width, _ = frame.shape
             now = datetime.datetime.now()
@@ -93,19 +50,18 @@ class Camera(BaseCamera):
                 2,
             )
             if prediction == 1:
-                # print("cul", prediction)
-                # print(f'\t\t|____No-Fire')
-                # cv2.rectangle(frame, (0, 0), (width, height), (0, 0, 255), 10)
-                cv2.putText(
-                    frame,
-                    "No-Fire",
-                    (int(width / 16), int(height / 4)),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    1,
-                    (0, 255, 00),
-                    2,
-                    cv2.LINE_AA,
-                )
+        
+                # cv2.putText(
+                #     frame,
+                #     "No-Fire",
+                #     (int(width / 16), int(height / 4)),
+                #     cv2.FONT_HERSHEY_SIMPLEX,
+                #     1,
+                #     (0, 255, 00),
+                #     2,
+                #     cv2.LINE_AA,
+                # )
+                print("no Fire")
             else:
                 # print("cul", prediction)
                 # print(f'\t\t|____Fire')
@@ -139,11 +95,11 @@ class Camera(BaseCamera):
                 else:
                   os.mkdir(path)    
 
-                today = datetime.datetime.now()
+                # today = datetime.datetime.now()
                 # print(today.strftime("%Y-%m-%d"))
 
                 # if int(localtime.second) % 2 == 0:
-                if (now != end) & (int(now - end) % 5 == 0):
+                if (now  % 59 == 0):
                     # print("time end",end)
                     # print(str(threading.current_thread().ident) + " Phat hien chay " + str(today))
                     cv2.rectangle(frame, (0, 0), (width, height), (0, 0, 255), 30)
@@ -152,8 +108,8 @@ class Camera(BaseCamera):
                     localtime.strftime("%d %H-%M-%S")
                     frame_name = localtime.strftime("%d %H-%M-%S") + ".jpg"
                     cv2.imwrite(os.path.join(out_path, frame_name), frame)
-                    name = "Phat hien chay"
-                    content = "co dam chay"
+                    name = "Phát hiện cháy"
+                    content = "Có cháy đang diễn ra"
                    
                     print("===========================",date_full )
                     end = datetime.datetime.now().second
@@ -172,30 +128,29 @@ class Camera(BaseCamera):
                     )
                     dec.save()
 
-                cv2.putText(
-                    frame,
-                    "Fire",
-                    (int(width / 16), int(height / 4)),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    1,
-                    (0, 0, 255),
-                    2,
-                    cv2.LINE_AA,
-                )
-
-            time_now = time.time()
-            total_time += time_now - time_start
-            fps = num_frames / total_time
+                # cv2.putText(
+                #     frame,
+                #     "Fire",
+                #     (int(width / 16), int(height / 4)),
+                #     cv2.FONT_HERSHEY_SIMPLEX,
+                #     1,
+                #     (0, 0, 255),
+                #     2,
+                #     cv2.LINE_AA,
+                # )
+            # time_now = time.time()
+            # total_time += time_now - time_start
+            # fps = num_frames / total_time
             # uncomment below to see FPS of camera stream\
 
-            cv2.putText(
-                frame,
-                "FPS: %.2f" % fps,
-                (int(20), int(40 * 5e-3 * frame.shape[0])),
-                0,
-                2e-3 * frame.shape[0],
-                (255, 255, 255),
-                2,
-            )
+            # cv2.putText(
+            #     frame,
+            #     "FPS: %.2f" % fps,
+            #     (int(20), int(40 * 5e-3 * frame.shape[0])),
+            #     0,
+            #     2e-3 * frame.shape[0],
+            #     (255, 255, 255),
+            #     2,
+            # )
 
             yield cam_id, frame, prediction
